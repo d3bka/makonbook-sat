addd73db00df23317e24aea4a9657cfe3ec53327
d3f4ult0-dev0ps <turgunboyevbaxadir@gmail.com>
2026-03-13 01:11:27 +0500

    Restructure SAT module: practice_tests page, vocabulary and admissions sections added

--- apps/sat/templatetags/auth_extras.py
+++ apps/sat/templatetags/auth_extras.py
@@ -4,6 +4,7 @@ from django.contrib.auth.models import Group
 register = template.Library()
 
 @register.filter(name='has_group')
-def has_group(user, group_name): 
-    group = Group.objects.get(name=group_name) 
-    return True if group in user.groups.all() else False
+def has_group(user, group_name):
+    if not user or not user.is_authenticated:
+        return False
+    return user.groups.filter(name=group_name).exists()
