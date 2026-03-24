2cc9b07e008719122f373963b9f00a18a6b82482
d3f4ult0-dev0ps <turgunboyevbaxadir@gmail.com>
2026-03-24 10:23:52 +0500

    Fixed Bugs

--- satmakon/settings.py
+++ satmakon/settings.py
@@ -69,7 +69,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    #'apps.sat.middleware.ClientSoftwareMiddleware',
+    'apps.sat.middleware.ClientSoftwareMiddleware',
 ]
 
 ROOT_URLCONF = 'satmakon.urls'
